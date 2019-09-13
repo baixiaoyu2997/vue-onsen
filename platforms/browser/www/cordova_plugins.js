69f1c0476016fc49026a69b575dbf71b2be55fdb
L.Rain <bxy2997@sina.com>
2019-09-13 15:36:49 +0800

    修改ignore规则

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,10 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-geolocation": "4.0.2"
+}
+// BOTTOM OF METADATA
+});
