5ab800001906d83fa4125617031bfdc738de4f41
L.Rain <bxy2997@sina.com>
2019-09-15 08:32:58 +0800

    优化findVideo页样式

--- web/src/pages/findVideoMenu/FindVideo.vue
+++ web/src/pages/findVideoMenu/FindVideo.vue
@@ -17,13 +17,8 @@ export default {
 </script>
 <style lang="scss" scoped>
 .content{
-    margin: auto;
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    width: 3em;
-    height: 1.5em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 </style>
