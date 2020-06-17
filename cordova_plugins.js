2a387f71edc5c9bc542c1c4c484ed2a20b2d7c96
baixiaoyu2997 <bxy2997@sina.com>
2020-06-17 11:59:46 +0000

    Deploying to gh-pages from master 18493b81e518ceaf0cd91f5a97de3dae88e06051

--- cordova_plugins.js
+++ cordova_plugins.js
@@ -14,8 +14,8 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.4",
-    "cordova-plugin-baidumaplocation": "4.0.2"
+    "cordova-plugin-baidumaplocation": "4.0.2",
+    "cordova-plugin-whitelist": "1.3.4"
 }
 // BOTTOM OF METADATA
 });
