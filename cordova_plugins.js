f3b9cffe2f0e8a1150516df63965355c76bf9fbd
L.Rain <bxy2997@sina.com>
2020-06-17 19:57:26 +0800

    fix(影片详情): 修复v-for和v-show拆分使用，防止属性获取不到

---
+++ cordova_plugins.js
@@ -0,0 +1,21 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-baidumaplocation/www/baidumap_location.js",
+        "id": "cordova-plugin-baidumaplocation.baidumap_location",
+        "pluginId": "cordova-plugin-baidumaplocation",
+        "clobbers": [
+            "cordova.plugins.baidumap_location",
+            "plugin.baidumap_location",
+            "baidumap_location"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-baidumaplocation": "4.0.2"
+}
+// BOTTOM OF METADATA
+});
